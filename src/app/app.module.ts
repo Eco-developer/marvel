01ccaeaffec528fe890583c1386e06f4a16c9333
Jose Quesada <josesk8mqc@gmail.com>
2023-09-21 11:30:29 -0400

    agregar paginas al Router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComicsComponent } from './comics/comics.component';
+import { HomeComponent } from './home/home.component';
+import { ComicsDetailsComponent } from './comics-details/comics-details.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CharactersDetailsComponent } from './characters-details/characters-details.component';
+import { CreatorsComponent } from './creators/creators.component';
+import { CreatorsDetailsComponent } from './creators-details/creators-details.component';
+import { SeriesComponent } from './series/series.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComicsComponent,
+    HomeComponent,
+    ComicsDetailsComponent,
+    CharactersComponent,
+    CharactersDetailsComponent,
+    CreatorsComponent,
+    CreatorsDetailsComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
