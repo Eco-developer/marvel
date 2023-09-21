7ca97e766accd27c763e03bb013bd97db66f8385
Jose Quesada <josesk8mqc@gmail.com>
2023-09-21 11:29:05 -0400

    crear pagina de comics details.

---
+++ src/app/comics-details/comics-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comics-details',
+  templateUrl: './comics-details.component.html',
+  styleUrls: ['./comics-details.component.css']
+})
+export class ComicsDetailsComponent {
+
+}
